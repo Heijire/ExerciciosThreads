cd90f05563538effe81a2a1b3db9e668d52e91e2
heiji <heiji@debian>
2024-03-14 08:25:51 -0400

    test

--- src/Controller/Linux.java
+++ src/Controller/Linux.java
@@ -18,31 +18,38 @@ import java.io.InputStreamReader;
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+	    	
 	    	System.out.println(serv);
-	        try {
+	        if(serv.contains("google")){
+	        	serv = "Google";
+	        }else if(serv.contains("uol")){
+	        	serv = "UOL";
+	        }else if(serv.contains("terra")) {
+	        	serv= "Terra";
+	        }
+	        	try {
+	        
 	            Process proc = Runtime.getRuntime().exec(comando);
 	            InputStream fluxo = proc.getInputStream(); 
 	            InputStreamReader leitor = new InputStreamReader(fluxo);
 	            BufferedReader buffer = new BufferedReader(leitor);
 	            String linha = buffer.readLine();
+	            double tempo, total=0 ,media=0;
+	            int vezes= 0;
 	            while (linha != null) {
-	            	
-	            	
-	            	
-	            	//	                if (linha.contains("time=")) {
-//	                    int indiceInicio = linha.indexOf("time=") + 5;
-//	                    int indiceFim = linha.indexOf(" ", indiceInicio);
-//	                    String tempoStr = linha.substring(indiceInicio, indiceFim);
-//	                    int tempo = Integer.parseInt(tempoStr);
-//	                    somaTempo += tempo;
-//	                    contador++;
-//	                    System.out.println(serv + ": Iteração " + contador + ", Tempo: " + tempo + "ms");
-//	                }
-	            }
-//
-//	            int tempoMedio = somaTempo / contador;
-//	            System.out.println("Tempo médio para " + serv + ": " + tempoMedio + "ms");
-
+	            	if(linha.contains("time=")) {
+		            	String[] parts = linha.split("time=");
+		            	if ( parts.length> 1) {
+		            		String [] num = parts[1].split("\\s+");
+		            		tempo = Double.parseDouble(num[0]);
+		            		total = total+tempo;
+		            		vezes = vezes + 1; 
+		            		System.out.println(serv+ " - levou o tempo de " + tempo + "ms, para fazer 1 interação");
+		            	}
+	            	}
+	            }	            
+	            media = total /vezes;
+	            System.out.println(serv+ " - O tempo medio é de " + media + "ms");
 	        } catch (IOException | NumberFormatException e) {
 	            e.printStackTrace();
 	        }
